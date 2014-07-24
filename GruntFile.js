38182bae371f8d5a71db8875698d4a12daeebf38
MeredithU <me@meredithunderell.com>
2014-07-23 22:03:57 -0700

    Added a README file

--- GruntFile.js
+++ GruntFile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
 					compass: false
 				},
 				files: {
-					'<%= project.assets %>/css/style.css':'<%= project.css %>',
+					'<%= project.assets %>/css/style.css':'<%= project.css %>'
 				}
 			}
 		},
---
+++ README.md
@@ -0,0 +1,3 @@
+# AngularJS Animation Single Page App
+
+## Single page app using AngularJS and the Greensock animation platform to build animations. It also includes Bower & Grunt.
