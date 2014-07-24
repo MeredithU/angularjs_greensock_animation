73fcb986aa482e920f7d28cc4b6f154569f180ef
MeredithU <me@meredithunderell.com>
2014-07-23 19:48:33 -0700

    Trying to get Grunt watch working

---
+++ GruntFile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		project: {
+			app: ['app'],
+			assets: ['<%= project.app %>/assets'],
+			css: ['<%= project.assets %>/sass/style.scss']
+		},
+		sass: {
+			dev: {
+				options: {
+					style: 'expanded',
+					compass: false
+				},
+				files: {
+					'<%= project.assets %>/css/style.css':'<%= project.css %>',
+				}
+			}
+		},
+		watch: {
+			sass: {
+				files: '<%= project.assets %>/sass/{,*/}*.{scss,sass}',
+				tasks: ['sass:dev']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', [
+		'watch'
+	]);
+
+};
